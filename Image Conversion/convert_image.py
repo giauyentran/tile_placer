9cc717a18d7ecb16410aeb012aa84cfe778696d0
giauyentran <giauyen03@gmail.com>
2022-12-05 18:22:11 -0500

    define image conversion as function

--- Image Conversion/convert_image.py
+++ Image Conversion/convert_image.py
@@ -1,27 +1,31 @@
 from PIL import Image
 from numpy import asarray
 
-# convert to grayscale
-img = Image.open('test_images/test_8.jpeg').convert('L')
+def image_to_array(image_path):
 
-# reduce resolution
-img = img.resize((24,18))
+    # convert to grayscale
+    img = Image.open(image_path).convert('L')
+    img.show()
 
-# convert to 2D array
-img_array = asarray(img)
-dimensions = img_array.shape
+    # reduce resolution
+    img = img.resize((24,18))
+    img.show()
 
-# set threshold for binary pixels
-threshold = 200
+    # convert to 2D array
+    img_array = asarray(img)
+    dimensions = img_array.shape
 
-# convert to binary array
-for j in range(dimensions[1]):
-    for i in range(dimensions[0]):
-        if img_array[i][j] >= threshold:
-            img_array[i][j] = 1
-        else:
-            img_array[i][j] = 0
+    # set threshold for binary pixels
+    threshold = 125
 
-# convert array into binary array
-binary_img = Image.fromarray(img_array * 255 , 'L')
-binary_img.show()
+    # convert to binary array
+    for j in range(dimensions[1]):
+        for i in range(dimensions[0]):
+            if img_array[i][j] >= threshold:
+                img_array[i][j] = 1
+            else:
+                img_array[i][j] = 0
+
+    # convert array into binary array
+    binary_img = Image.fromarray(img_array * 255 , 'L')
+    binary_img.show()
