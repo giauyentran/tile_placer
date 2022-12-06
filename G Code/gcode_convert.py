06c829f8462dad9eb486067179181525fb5b9ae9
giauyentran <giauyen03@gmail.com>
2022-12-06 00:19:29 -0500

    fix pump command

--- G Code/gcode_convert.py
+++ G Code/gcode_convert.py
@@ -32,20 +32,21 @@ def gcode_move_xy(coords):
     '''
     return(f"G1 X{coords[0]} Y{coords[1]}\n")
 
-def gcode_pump(vacuum_pin, state):
+def gcode_pump(state):
     # TODO: confirm this Gcode command
     '''
     Generated Gcode command to move toolhead to location 
 
     Args:
         coords: A 2-tuple with the (x,y) cartesian coordinate of the target location
+        state: a string describing the desired state of the pump, either "OFF" or "ON"
 
     Returns a string with the Gcode command to move to coords.
     '''
 
-    if state == "LOW":
-        return(f"SET_PIN PIN = {vacuum_pin} VALUE = LOW\n")
-    elif state == "HIGH":
-        return(f"SET_PIN PIN = {vacuum_pin} VALUE = HIGH\n")
+    if state == "OFF":
+        return("TURN_OFF_PUMP\n")
+    elif state == "ON":
+        return("TURN_ON_PUMP\n")
     else:
-        return("Error: Pump state value invalid")
+        return("Error: Pump state invalid")
