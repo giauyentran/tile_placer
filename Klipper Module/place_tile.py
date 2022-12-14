b96bba365dc257fb871513746a36a6f2d8a76e4d
drewnotdrew <53927275+drewnotdrew@users.noreply.github.com>
2022-12-14 15:51:04 -0500

    Update for config from MK3 to Untiled
    :white_large_square::black_large_square::white_large_square::black_large_square::white_large_square::black_large_square::white_large_square::black_large_square::white_large_square::black_large_square::white_large_square::black_large_square::white_large_square::black_large_square::white_large_square::black_large_square:

--- Klipper Module/place_tile.py
+++ Klipper Module/place_tile.py
@@ -1,12 +1,12 @@
 # Global variables
 PLACEMENT_SPEED = 100.
 TILE_THICKENESS_THRESHOLD = 2.
-FLIPPER_PICKUP_POSITION = (150,150) # (15.5,409.) # Final, testing with MK3 build area
+FLIPPER_PICKUP_POSITION = (15.5,409.) # Final, testing with MK3 build area
 XY_TRAVEL = 600
 Z_TRAVEL = 400
 
 # Pin definitions
-SOLENOID_PIN = "PH5" # "PA14" Final, testing with MK3 fan pin
+# SOLENOID_PIN = "PA14" # Final, testing with MK3 fan pin :P
 
 class EndstopWrapper:
     def __init__(self, config, endstop):
@@ -25,7 +25,7 @@ class PrinterPlaceProbe:
         self.printer = config.get_printer()
         self.pins = self.printer.lookup_object("pins")
         #self.pins.reset_pin_sharing("SOLENOID_PIN") # Need initially, not sure how long this is persistent
-        self.solenoid = self.pins.setup_pin("pwm", SOLENOID_PIN)
+        #self.solenoid = self.pins.setup_pin("pwm", SOLENOID_PIN)
         self.position_min = config.getfloat("position_min", None)
         self.gcode = self.printer.lookup_object("gcode")
         self.query_endstops = self.printer.load_object(config,
