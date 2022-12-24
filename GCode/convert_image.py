faba5f31a0272c001340daf71d62c95784008212
giauyentran <giauyen03@gmail.com>
2022-12-24 16:27:48 -0600

    fix image inversion and indexing

--- GCode/convert_image.py
+++ GCode/convert_image.py
@@ -30,18 +30,22 @@ def image_to_array(image_path, image_dimensions):
     dimensions = img_array.shape
 
     # set threshold for binary pixels
-    threshold = 150
+    threshold = 110
 
     # convert to binary array
     for j in range(dimensions[1]):
         for i in range(dimensions[0]):
             if img_array[i][j] >= threshold:
-                img_array[i][j] = 0
-            else:
                 img_array[i][j] = 1
+            else:
+                img_array[i][j] = 0
+    
+    # flip to normalize indexing
+    # array indexes from top left pixel, but gantry indexes from bottom left
+    img_array = np.flipud(img_array)
 
-    # Uncomment next 2 lines to display final binary grid
-    binary_img = Image.fromarray(img_array * 255, 'L')
-    binary_img.show()
+    # Uncomment next 2 lines to display final binary image
+    # binary_img = Image.fromarray(np.flipud(img_array) * 255, 'L')
+    # binary_img.show()
     
     return img_array
